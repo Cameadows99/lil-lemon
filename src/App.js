b796b4f3385ff63ed350271f25521d7808751af2
Chase <cmeadows_17@yahoo.com>
2025-05-19 21:27:35 -0400

    added og meta tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Header } from './components/Header';
+import { Nav } from './components/Nav';
+import { Body } from './components/Body';
+import { Footer } from './components/Footer';
+import logo from './assets/logo.png';
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+    <Header />
+    <Nav />
+    <Body />
+    <Footer />
+    </>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import logo from '../assets/logo.png'
+
+export const Nav = () => {
+
+    return (
+        <>
+        <img src={logo} alt="logo" style={{height: 100, width: 500}} />
+              <ul>
+                <li><a></a></li>
+                <li><a></a></li>
+                <li><a></a></li>
+              </ul>
+        </>
+    )
+}
+
